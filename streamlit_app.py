389edb65e192cc5007664476f800a66622e8ead5
indapa <indapa@gmail.com>
2023-05-15 23:33:20 -0700

    updated datafile and source

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import sys
 
 
 
-solar_file = Path(__file__).parent / "solar_production.csv"
+solar_file = Path(__file__).parent / "solar_production_2023.csv"
 # Load data
 data = pd.read_csv(solar_file)
 # Streamlit app code
