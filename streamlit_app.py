c054b8427a3c444a43fb2afed634210b7ba7a448
indapa <indapa@gmail.com>
2023-06-20 21:47:59 -0700

    updated to add cumulative power plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ import sys
 solar_file = Path(__file__).parent / "solar_production_2023.csv"
 # Load data
 data = pd.read_csv(solar_file)
+data['cumulative']=data['Production'].cumsum()
 # Streamlit app code
 def main():
     st.title("Solar Panel Power Production")
@@ -20,9 +21,17 @@ def main():
     st.dataframe(data)
 
     # Plotting the data
-    st.subheader("Solar Production 2023")
+    st.subheader("Daily Solar Production 2023")
     plot_power_production(data)
 
+
+    # Plotting cumulative power production
+    st.subheader("Cumulative Solar Production 2023")
+    plot_cumulative_power_production(data)
+
+
+
+    
 # Function to plot power production 
 def plot_power_production(data):
     data['Time'] = pd.to_datetime(data['Time'])  # Convert to datetime format
@@ -40,6 +49,17 @@ def plot_power_production(data):
     plt.xticks(rotation=90)  # Rotate x-axis labels by 90 degrees
     plt.grid(True)
     st.pyplot(plt)
+
+# function to plot cumulative power production in seaborn
+def plot_cumulative_power_production(data): 
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(x='Time', y='cumulative', data=data)
+    plt.xlabel('Time')
+    plt.ylabel('Cumulative Power (kWh)')
+    plt.title('Cumulative Solar Panel Power Production')
+    plt.grid(True)
+    st.pyplot(plt)
+  
 
 if __name__ == '__main__':
     main()
